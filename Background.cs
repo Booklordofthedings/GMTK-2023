cd995b4ac2f2eb0ec77f1fae61a4a6ed10918670
Booklordofthedings <jannis.v.hagen@gmail.com>
2023-07-07 21:41:39 +0200

    scrolling texture

--- Background.cs
+++ Background.cs
@@ -4,35 +4,52 @@ using System;
 public partial class Background : Node2D
 {
 	[Export]
-	float speed = 1;
+	float ScrollSpeed = 200;
 	
 	[Export]
-	Node2D element1;
+	Node2D fst;
 	[Export]
-	Node2D element2;
+	Node2D lst;
 	
-	private float startingBack;
-	private float startingFront;
+	private float start; //Point at which the first element is reset
+	private float end; //Point at which the last element is reset
+	private bool first = true;
 	
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		startingFront = element1.Positon.X;
-		startingBack = element2.Position.X;
+		start = fst.Position.X;
+		end = lst.Position.X;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		MoveWithSpeed(element1,delta);
-		MoveWithSpeed(element2,delta);
+		fst.Position = MoveWithSpeed(fst,delta);
+		lst.Position = MoveWithSpeed(lst,delta);
+		if(first)
+		{
+				if(lst.Position.X >= start)
+				{
+					fst.Position = new Vector2(end,lst.Position.Y);
+					first = false;
+				}
+		}
+		else
+		{
+				if(fst.Position.X >= start)
+				{
+					lst.Position = new Vector2(end,fst.Position.Y);
+					first = true;
+				}
+		}
+	
 		
-		if(element1.Po)		
 	}
 	
 	private Vector2 MoveWithSpeed(Node2D pVar, double delta)
 	{
-		pVar.Positon = new Vector2(pVar.Position.X + speed * (float)delta,pVar.Position.Y);
+		return new Vector2(pVar.Position.X + ScrollSpeed * (float)delta,pVar.Position.Y);
 	}
 }
