b0731dd2854e0dcfae5124ffa4cb3e1b1d30d855
Li0nMind <leo.voelkner@gmail.com>
2023-07-08 19:10:45 +0200

    modify king

--- Game/King/script/foolishKing.gd
+++ Game/King/script/foolishKing.gd
@@ -4,12 +4,8 @@ extends CharacterBody2D
 @onready var animations = $AnimationPlayer
 var direction = 1;
 
-#func get_input():
-#	var input_direction = Input.get_vector("left", "right", "up", "down")
-#	velocity = input_direction * speed
-
-func _process(_delta):
-		animations.play("idle")
+#func _process(_delta):
+#		animations.play("idle")
 
 func _physics_process(delta):
 	if position.y > 850:
@@ -22,10 +18,10 @@ func _physics_process(delta):
 	velocity += Vector2(0, 0.2 * direction)
 	velocity.y = clamp(velocity.y, -30, 30)
 	
-	if direction == -1:
-		animations.play("walkUp")
-	else:
+	if direction == 1:
 		animations.play("walkDown")
+	else:
+		animations.play("walkUp")
 		
 	move_and_slide()
 	
