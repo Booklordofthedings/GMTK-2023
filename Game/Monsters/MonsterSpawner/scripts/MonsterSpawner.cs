1b20abb64634bf76d51fb3123460380f4babf124
Booklordofthedings <jannis.v.hagen@gmail.com>
2023-07-08 20:29:22 +0200

    mroe

--- Game/Monsters/MonsterSpawner/scripts/MonsterSpawner.cs
+++ Game/Monsters/MonsterSpawner/scripts/MonsterSpawner.cs
@@ -20,20 +20,7 @@ public partial class MonsterSpawner : Node
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-<<<<<<< HEAD
-		for(int i = 0; i < Monsters.Length; i++)
-		{
-			for(int ii = 0; ii < Weights[i]; ii++)
-			{
-				Items.Add(Monsters[i]);
-			}
-		}
-		
-		
-//		NewWave();
-=======
 		NewWave();
->>>>>>> 3959554 (spawner works now)
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -52,24 +39,12 @@ public partial class MonsterSpawner : Node
 
 		int WavePower = 2 + (int)(Wave * 1.5f);
 		WaveLabel.Text = "Wave:" + Wave.ToString();
-<<<<<<< HEAD
-		while(true)
-		{
-			int WavePower = (int)(5 + Wave + Mathf.Pow(Wave, 1.5));
-			var ran = new Random();
-			var toSpawn = ran.Next(Items.Count);
-			WavePower -= toSpawn+1;
 
-			AddChild(Items[toSpawn].Instantiate());
-			if(WavePower < 0)
-				return;
-=======
 
 		Random ran = new Random();
 		for(int i = 0; i < WavePower; i++)
 		{
 			TimerFunction(ran, WavePower, i);
->>>>>>> 3959554 (spawner works now)
 		}
 		
 	}
