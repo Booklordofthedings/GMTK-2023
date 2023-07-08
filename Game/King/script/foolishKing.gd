decfa0e6e3966e6e44fe40ca5741f8d5daebc5f4
Booklordofthedings <jannis.v.hagen@gmail.com>
2023-07-09 01:32:13 +0200

    end game feature

--- Game/King/script/foolishKing.gd
+++ Game/King/script/foolishKing.gd
@@ -3,19 +3,19 @@ extends RigidBody2D
 @export var speed = 400
 @onready var animations = $AnimationPlayer
 var direction = 1;
-var timer = 1.5;
+var timer = 0.5;
 
 func _physics_process(delta):
 	timer -= delta
 	if(timer < 0):
-			(get_node("CollisionShape2D") as CollisionShape2D).disabled = false
+			(get_node("CollisionShape2D") as CollisionShape2D).set_deferred("disabled",false)
 	
 	if position.y > 950:
 		direction = -1
 	elif position.y < 50:
 		direction = 1
 	
-	move_and_collide(Vector2(1,direction * delta * 500))
+	move_and_collide(Vector2(0,direction * delta * 500))
 	
 	if direction == 1:
 		animations.play("walkDown")
@@ -23,13 +23,13 @@ func _physics_process(delta):
 		animations.play("walkUp")
 		
 	if(position.x < -10):
-		get_tree().change_scene_to_file("res://Menus/DeathMenu/DeathMenu.tscn")
+		get_tree().change_scene_to_file("res://Menus/Death/Death_Menu.tscn")
 	
 
 
 
 func _on_body_entered(body):
-	(get_node("CollisionShape2D") as CollisionShape2D).disabled = true
+	(get_node("CollisionShape2D") as CollisionShape2D).set_deferred("disabled",true)
 	direction = direction * -1
-	timer = 1.5
+	timer = 0.5
 	pass # Replace with function body.
