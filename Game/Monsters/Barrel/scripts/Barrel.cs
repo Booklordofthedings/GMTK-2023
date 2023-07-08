e243d1eb222f19c0adb06f946987c6072b84af7e
Booklordofthedings <jannis.v.hagen@gmail.com>
2023-07-08 22:44:11 +0200

    barrels spawning and doing

---
+++ Game/Monsters/Barrel/scripts/Barrel.cs
@@ -0,0 +1,20 @@
+using Godot;
+using System;
+
+public partial class Barrel : RigidBody2D
+{
+	[Export] Vector2 ToMove;
+	[Export] Sprite2D ToRotate;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		Position = new Vector2(2000, Mathf.Clamp(new Random().Next(1000),100,1000));
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		MoveAndCollide(ToMove * (float)delta);
+		ToRotate.Rotate(-1f * (float)delta);
+	}
+}
