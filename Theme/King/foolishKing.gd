f0f694d912463387e7f165d3a5ca10f8dd54097b
Fabio <fabio.voelkner@gmail.com>
2023-07-08 15:00:38 +0200

    add enemy movement

--- Theme/King/foolishKing.gd
+++ Theme/King/foolishKing.gd
@@ -3,9 +3,9 @@ extends CharacterBody2D
 @export var speed = 400
 @onready var animations = $AnimationPlayer
 
-func get_input():
-	var input_direction = Input.get_vector("left", "right", "up", "down")
-	velocity = input_direction * speed
+#func get_input():
+#	var input_direction = Input.get_vector("left", "right", "up", "down")
+#	velocity = input_direction * speed
 
 func _process(_delta):
 	if Input.is_action_pressed("down"):
@@ -13,8 +13,8 @@ func _process(_delta):
 	elif Input.is_action_pressed("up"):
 		animations.play("walkUp")
 	else:
-		animations.stop()
+		animations.play("idle")
 
 func _physics_process(delta):
-	get_input()
+#	get_input()
 	move_and_slide()
