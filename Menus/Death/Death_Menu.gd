9d35b51e54d35ee386f09395ddfabfac9605492d
Li0nMind <leo.voelkner@gmail.com>
2023-07-09 18:52:38 +0200

    death menu to gdscript

---
+++ Menus/Death/Death_Menu.gd
@@ -0,0 +1,21 @@
+extends Control
+
+@export var toLabel : Label
+
+
+func _ready():
+	var globals = get_node("/root/Globals")
+	var score = int(globals.get("score"))
+	var highscore = int(globals.call("GetHighscore"))
+	
+	toLabel.text = "Score: " + str(score) + "\n" + "Highscore: " + str(highscore)
+
+func _process(delta: float):
+	pass
+
+func _on_resume_btn_pressed():
+	get_tree().change_scene("res://Game/MainGame/Game.tscn")
+
+func _on_end_pressed():
+	get_tree().change_scene("res://Menus/MainMenu/MainMenu.tscn")
+
