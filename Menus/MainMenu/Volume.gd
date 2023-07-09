9d6bdabaf8e5dc87ef0e966fc0c323f540d3a38b
Li0nMind <leo.voelkner@gmail.com>
2023-07-09 15:16:55 +0200

    fix volume slider

--- Menus/MainMenu/Volume.gd
+++ Menus/MainMenu/Volume.gd
@@ -2,4 +2,3 @@ extends HSlider
 
 func _on_value_changed(value):
 	Globals.SetMasterVolume(value)
-	get_node("../../../../../../PauseMenu/MarginContainer/VBoxContainer/MarginContainer/VBoxContainer/HSlider").set_value_no_signal(value)
--- Menus/PauseMenu/HSlider.gd
+++ Menus/PauseMenu/HSlider.gd
@@ -1,5 +1,5 @@
-extends HSlider
-
-func _on_value_changed(value):
-	Globals.SetMasterVolume(value)
-	get_node("../../../../../../MainMenu/StartOptions/Container/TitleOptions/Containers/Volume").set_value_no_signal(value)
+#extends HSlider
+#
+#func _on_value_changed(value):
+#	Globals.SetMasterVolume(value)
+#	get_node("../../../../../../MainMenu/StartOptions/Container/TitleOptions/Containers/Volume").set_value_no_signal(value)
