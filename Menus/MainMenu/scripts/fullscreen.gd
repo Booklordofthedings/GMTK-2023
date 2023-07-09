590fd508b88f372101fd78df298a51a2966be79b
Booklordofthedings <jannis.v.hagen@gmail.com>
2023-07-09 18:34:48 +0200

    fix

--- Menus/MainMenu/scripts/fullscreen.gd
+++ Menus/MainMenu/scripts/fullscreen.gd
@@ -6,12 +6,5 @@ func _ready():
 func _process(delta: float):
 	pass
 	
-<<<<<<< HEAD
-#func toggle_fullscreen(state: bool):
-func _on_toggled(state: bool):
-	DisplayServer.window_set_mode(state if DisplayServer.WINDOW_MODE_FULLSCREEN else DisplayServer.WINDOW_MODE_WINDOWED)
-=======
 func toggle_fullscreen(state: bool):
 	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN if state else DisplayServer.WINDOW_MODE_WINDOWED)
-
->>>>>>> 0049db6 (fullscreen fix)
